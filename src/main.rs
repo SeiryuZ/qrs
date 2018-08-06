c0ae1ed5ba36d025889b507d2834abcea8e980b0
Steven <stvn.bu@gmail.com>
2018-08-06 19:27:54 +0700

    Add flags and test trust release again

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,51 @@ fn main() {
             Arg::with_name("key")
                 .short("k")
                 .long("secret-key")
-                .value_name("KEY")
-                .help("Secret Key for the encryption")
+                .value_name("TOKEN")
+                .help("Secret Key(Token) provided from merchant interface")
                 .takes_value(true),
-        ).get_matches();
+        )
+        .arg(
+            Arg::with_name("Merchant ID")
+                .short("m")
+                .long("merchant-id")
+                .value_name("MERCHANT_ID")
+                .help("Merchant ID provided from merchant interface")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Store ID")
+                .short("s")
+                .long("store-id")
+                .value_name("STORE_ID")
+                .help("Store ID provided from merchant interface")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Invoice Number")
+                .short("i")
+                .long("invoice-number")
+                .value_name("INVOICE_NUMBER")
+                .help("Invoice Number for this transaction")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Value")
+                .short("v")
+                .long("value")
+                .value_name("TRANSACTION_VALUE")
+                .help("Value of this transaction")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Timestamp")
+                .short("t")
+                .long("timestamp")
+                .value_name("Timestamp")
+                .help("Unix timestamp of transaction")
+                .takes_value(true),
+        )
+        .get_matches();
 
     println!("Hello, world!");
 }
