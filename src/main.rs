00a547c5ab7ffdbe15cb0f680a5812e9935e05ef
Steven <stvn.bu@gmail.com>
2018-08-06 19:00:31 +0700

    Test trust release

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
+    let matches = App::new("QRs code generator")
+        .version("0.1.0")
+        .author("Steven <steven@ui.co.id>")
+        .about("Generate QR code for transaction information")
+        .arg(
+            Arg::with_name("key")
+                .short("k")
+                .long("secret-key")
+                .value_name("KEY")
+                .help("Secret Key for the encryption")
+                .takes_value(true),
+        ).get_matches();
+
     println!("Hello, world!");
 }
